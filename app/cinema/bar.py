5153d1929f990b367277ddc671bfc80f2cd28468
Krzysztof Hajlasz <krhajlas@cisco.com>
2026-01-22 13:49:11 +0100

    Updates 1

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -5,5 +5,6 @@ class CinemaBar():
     def __init__(self) -> None:
         pass
 
-    def sell_product(self, customer: Customer, product: str) -> None:
-        print(f"Cinema bar sold {product} to {customer.name}")
+    @staticmethod    
+    def sell_product(customer: Customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.people.customer import Customer
-from app.people.cinema_stuff import Cleaner
+from app.people.cinema_staff import Cleaner
 from app.cinema.hall import CinemaHall
 from app.cinema.bar import CinemaBar
 
