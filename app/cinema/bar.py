77eb1acb54693519f8173422677ab27b3aca833a
Krzysztof Hajlasz <krhajlas@cisco.com>
2026-01-22 13:50:24 +0100

    Updates 2: flake8

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -5,6 +5,6 @@ class CinemaBar():
     def __init__(self) -> None:
         pass
 
-    @staticmethod    
+    @staticmethod
     def sell_product(customer: Customer, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
